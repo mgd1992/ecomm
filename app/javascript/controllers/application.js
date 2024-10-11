f6c56a6b047a021c98a1fd81483da74328121c2c
marco garbini <marcoogarbini@gmail.com>
2024-10-11 14:15:37 +0200

    ecommerce project full stack

---
+++ app/javascript/controllers/application.js
@@ -0,0 +1,10 @@
+import { Application } from "@hotwired/stimulus"
+import Rails from '@rails/ujs';
+const application = Application.start()
+Rails.start();
+
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus   = application
+
+export { application }
---
+++ config/importmap.rb
@@ -0,0 +1,9 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "chart.js" # @4.4.4
+pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.2
