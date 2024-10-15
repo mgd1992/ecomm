057818a1dab665240bb2e5eafb0d07d1330a58e2
marco garbini <marcoogarbini@gmail.com>
2024-10-15 13:29:08 +0200

    crate index page for users, showing some categories, reviews, the subscribe bottom, and the footer

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,12 +1,9 @@
 import { Application } from "@hotwired/stimulus"
-import { registerables } from "chart.js" // Import Chart.js
 
-import DashboardController from "controllers/dashboard_controller" // Import your controller
-
-// Initialize Stimulus
 const application = Application.start()
-application.register("dashboard", DashboardController) // Register the Stimulus controller
 
-// Register Chart.js components
-import { Chart } from "chart.js"
-Chart.register(...registerables)
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus   = application
+
+export { application }
